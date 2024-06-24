efb727193abf72d15ca065815cc93ab15d48a995
root <root@trader.improbability.io>
2024-06-24 04:27:26 +0000

    Update

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from config import Config
-from models import db 
 from utils.logger import setup_logger
 from routes.routes import configure_routes  
 from werkzeug.middleware.proxy_fix import ProxyFix
-import stripe
 
 def create_app():
     app = Flask(__name__)
@@ -19,16 +17,9 @@ def create_app():
     logger = setup_logger('app_logger',app.config['LOG_PATH'])
     app.logger.addHandler(logger)
     
-    # Set Stripe API Key
-    stripe.api_key = app.config['STRIPE_API_KEY']
-
     # Configure Routes
     configure_routes(app)
 
-    # Create tables if they do not exist
-    with app.app_context():
-        db.create_all()
-        
     return app
 
 app = create_app()  # This line added
