80a99dbc53bce9e3a098f3d92376cc5eb039544e
Rahim Khoja <rahim@khoja.ca>
2024-06-23 21:33:37 -0700

    Update meta_tags.py

--- meta_tags.py
+++ meta_tags.py
@@ -1,4 +1,6 @@
 class MetaTags:
+    PRODUCT_NAME = "The Quant"
+    HOST_NAME = "quant.improbability.io"
     KEYWORDS = "AI SMS Chatbot, AI Assistant, What is AI assistance, Personal AI Assistant, AI Assistant App, SMS, SMS AI, AI Assisted Text Messaging, AI Text Messaging, Pocket AI, SMS Chatbot, SMS AI Chatbot, AI Text Message Assistant, Custom AI assistant, Multilingual AI, No Data AI, Improbability Labs, Improbability"
     DESCRIPTION = "Your pocket AI companion - Explore, learn, decide—anywhere, anytime with your SMS AI Assistant. No internet? No problem! Personalized AI at your fingertips. Sign up for your personalized Ai Assistant today!"
     LINKEDIN = "https://www.linkedin.com/company/improbability-labs/"
